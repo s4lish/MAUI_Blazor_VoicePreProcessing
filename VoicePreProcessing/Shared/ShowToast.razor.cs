5a7bb81f6ec68614c1aebcee21cd66b44f125a91
s4lish <s4lish@gmail.com>
2023-03-27 15:42:17 +0330

    Add Custom Toast

---
+++ VoicePreProcessing/Shared/ShowToast.razor.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace VoicePreProcessing.Shared
+{
+    public partial class ShowToast
+    {
+
+        private string toastTitle;
+        private string toastMessage;
+        private string toastClass;
+        private string toastTime;
+        private bool openToast = false;
+        private int second = 4;
+
+        public ShowToast()
+        {
+            toastClass = GetToastClass(ToastType.Success);
+            toastTime = DateTime.Now.ToString("HH:mm");
+
+        }
+
+        [Parameter]
+        public EventCallback CloseToast { get; set; }
+
+        [Parameter]
+        public bool OpenToast
+        {
+            get { return openToast; }
+            set
+            {
+                openToast = value;
+                StateHasChanged();
+            }
+        }
+
+        [Parameter]
+        public string ToastClass
+        {
+            get { return toastClass; }
+            set
+            {
+                toastClass = value;
+                StateHasChanged();
+            }
+        }
+
+        [Parameter]
+        public string ToastTitle
+        {
+            get { return toastTitle; }
+            set
+            {
+                toastTitle = value;
+                StateHasChanged();
+            }
+        }
+
+        [Parameter]
+        public string ToastMessage
+        {
+            get { return toastMessage; }
+            set
+            {
+                toastMessage = value;
+                StateHasChanged();
+            }
+        }
+
+        private async Task HideToast()
+        {
+            //openToast = false;
+            await CloseToast.InvokeAsync();
+            StateHasChanged();
+        }
+
+        private string GetToastClass(ToastType type)
+        {
+            return type switch
+            {
+                ToastType.Success => "toast show bg-success",
+                ToastType.Info => "toast show bg-info",
+                ToastType.Warning => "toast show bg-warning",
+                ToastType.Error => "toast show bg-danger",
+                _ => "toast show"
+            };
+        }
+
+    }
+
+    public enum ToastType
+    {
+        Success,
+        Info,
+        Warning,
+        Error
+    }
+}
